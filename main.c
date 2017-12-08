635922c0461bc7fd0b8a4b6665d1fef96358fed1
Khinshan Khan <kkhan@stuy.edu>
2017-12-08 15:32:49 -0500

    main.c

--- main.c
+++ main.c
@@ -4,6 +4,10 @@ int main(){
   int v = sg_value();
   if(v){
     printf("Resources: %d\n", v);
+    printf("Last line: %s\n",  sg_line());
+    printf("Enter next line:");
+    char line[100];
+    fgets(line, sizeof(line), stdin);
   }
   else{
     printf("Resources: All are currently busy. Please standby.\n");
