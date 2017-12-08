13eed2a1a6da7087cd43311f982d1f66e877c08f
Kyle Lin <klni12@stuy.edu>
2017-12-08 13:03:28 -0500

    Initial Commit

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#endif
