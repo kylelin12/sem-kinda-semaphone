7192e2c75f69cea21a2383cf3e692f5653c98435
Khinshan Khan <kkhan@stuy.edu>
2017-12-10 09:41:34 -0500

    tried to make control more modular. added in shared memory

--- control.c
+++ control.c
@@ -5,16 +5,22 @@ void viewstory(){
   char * line = NULL;
   size_t num = 0;
   ssize_t read;
-  FILE *fp = fopen("storytext.txt", "r");
-  while ((read = getline(&line, &num, fp)) != -1) {
-    printf("%s", line);
+  FILE *fp = fopen("storytext", "r");
+  if(fp == NULL){
+    //story might not exist or something may have happened to path configs
+    printf("Error in trying to access storytext.txt\n");
   }
-  printf("\n");
-  fclose(fp);
+  else{
+    while ((read = getline(&line, &num, fp)) != -1) {
+      printf("%s", line);
+    }
+    printf("\n");
+    fclose(fp);
+  }
 }
 
-//creates a semaphore... or doesnt.
-void s_create(char * n){
+//creates a semaphore... or doesnt
+void semaphore_create(char * n){
   int id = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0664);
   // if -1, it existed, if not, semaphore didnt exist
   if (id != -1){
@@ -24,65 +30,107 @@ void s_create(char * n){
     semctl(id, 0, SETVAL, semaphore);
     printf("semaphore created: %d\n", id);
     printf("value set: %s\n", n);
-    //makes a new story
-    //wb+: Truncate to zero length or create file for update
-    FILE *fp = fopen("storytext.txt", "wb+");
-    fclose(fp);
-    printf("new storytext.txt created\n");
   } 
   else{
     // semaphore exists
-    printf("semaphore & a story already exist\n");
+    printf("semaphore already exists\n");
   }
 }
 
-//gets the value of the semaphore... or doesnt
-void s_value(){
-  int id = semget(KEY, 0, 0);
-  //error
-  if(id == -1){
-    printf("error: semaphore & story dont exist or no access to them\n");
+//creates a story... or doesnt.
+void story_create(){
+  //makes a new story
+  //wb+: Truncate to zero length or create file for update
+  FILE *fp = fopen("storytext.txt", "wb+");
+  if(fp == NULL){
+    //story might exist or something may have happened to path configs
+    printf("Error in trying to create a new storytext.txt\n");
   }
   else{
-    viewstory();
-    //printf("semaphore value: %d\n", semctl(id, 0, GETVAL));
+    fclose(fp);
+    printf("new storytext.txt created\n");
   }
+}
+
+//creates a shared memory... or doesnt.
+void sm_create(){
+  //create new shared memory
+  int sm = shmget(KEY, 1000, IPC_CREAT | IPC_EXCL); 
+  if (sm != -1){
+    shmat(sm, 0, 0);
+    printf("shared memory created: %d\n", KEY);
+  }
+  else{
+    printf("Shared memory exists\n");
+  }
+}
+
+//creates a semaphore, story, shared memory... or doesnt.
+void all_create(){
+  semaphore_create("1");
+  story_create();
+  sm_create();
+}
+
+//prints the story... or doesnt
+void s_value(){
+  viewstory();
 }
 
 //removes the semaphore... or doesnt
-void s_remove(){
+void semaphore_remove(){
   int id = semget(KEY, 0, 0);
-  //error
   if(id == -1){
     printf("error: semaphore doesnt exist or no access to it\n");
   }
   else{
-    viewstory();
-    if(remove("storytext.txt") == 0){
-      printf("Deleted successfully\n");
-    }
-    else{
-      printf("Unable to delete the story\n");
-    }
-    printf("semaphore removed %d\n", semctl(id, 0, IPC_RMID)); 
+    printf("semaphore removed %d\n", semctl(id, 0, IPC_RMID));
   }
 }
 
+//removes the story... or doesnt
+void story_remove(){
+  viewstory();
+  if(remove("storytext.txt") == 0){
+    printf("Deleted story successfully\n");
+  }
+  else{
+    printf("Unable to delete the story\n");
+  }
+}
+
+//removes the shared memory... or doesnt
+void sm_remove(){
+  int sm = shmget(KEY, 0, 0);
+  if(id == -1){
+    printf("error: shared doesnt exist or no access to it\n");
+  }
+  else{
+    printf("shared memory removed: %d\n", shmctl(sm, 0, IPC_RMID));
+  }
+}
+
+//removes the semaphore, story, shared memory... or doesnt
+void all_remove(){
+  semaphore_remove();
+  story_remove();
+  sm_remove();
+}
+
+
+
 int main(int argc, char *argv[]){
+  if(argc != 2){
+    printf("Invalid number of Arguments\n");
+    return 1;
+  }
   /*
-    -c N
-    Create a semaphore and set its value to N
-    If the semaphore already exists, it should pint a message saying so
+    -c
+    If the semaphore already exists, it should print a message saying so
   */
   if(!strcmp(argv[1],"-c")){
-    if(argc != 3){
-      printf("Invalid number of Arguments\n");
-      return 1;
-    }
-    else{
-      s_create(argv[2]);
-      return 0;
-    }
+    all_create();
+    return 0;
   }
 
   /*
@@ -90,10 +138,6 @@ int main(int argc, char *argv[]){
     View the current value of the semaphore
   */
   if(!strcmp(argv[1],"-v")){
-    if(argc != 2){
-      printf("Invalid number of Arguments\n");
-      return 1;
-    }
     s_value();
     return 0;
   }
@@ -103,11 +147,7 @@ int main(int argc, char *argv[]){
     remove the semaphore
   */
   if(!strcmp(argv[1],"-r")){
-    if(argc != 2){
-      printf("Invalid number of Arguments\n");
-      return 1;
-    }
-    s_remove();
+    all_remove();
     return 0;
   }
   
