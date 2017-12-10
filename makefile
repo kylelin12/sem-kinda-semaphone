cdbc9456854359b507425c95b9a81fb9b1bba368
Khinshan Khan <kkhan@stuy.edu>
2017-12-10 10:09:54 -0500

    fixed some more typos, started a basic write, and make a makefile that suit our purposes right now

--- makefile
+++ makefile
@@ -1,4 +1,19 @@
+all: control write
+	clear
+
 control:
 	gcc -o control control.c
+
 write:
 	gcc -o write write.c
+
+clean:
+	rm *~
+	clear
+
+cleanall:
+	rm control
+	rm write
+	clear
+	rm *~
+	clear
--- write.c
+++ write.c
@@ -1,1 +1,22 @@
 #include "main.h"
+
+static void sighandler(int signo){
+  //user wants to exit
+  if (signo == SIGINT){
+    printf("\nlogout\n");
+    exit(0);
+  }
+}
+
+int main(){
+  printf("Welcome to Semaphone! A game based off of Telephone, utilizing semaphores!\n\n ***Note: you can exit at anytime by simply pressing ctrl+C.\n\n");
+  printf("Rules of the game:\n1. Once in, we should display the last line added to the story\n2. We will prompt the use for the next line.\n");
+  while (1) {
+    signal(SIGINT, sighandler);
+    printf("\n[testing] Please input the next line:\n");
+    char line [100];
+    fgets(line, sizeof(line), stdin);
+    printf("\n%s\n",line);
+  }
+  return 0;
+}
